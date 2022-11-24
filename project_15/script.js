5bdbaab0b6dd83f652ec6412b5e4e8eecdb15827
SeydeSaw <115941526+SeydeSaw@users.noreply.github.com>
2022-11-24 14:26:59 +0100

    Add files via upload

--- project_15/script.js
+++ project_15/script.js
@@ -14,7 +14,7 @@ let products = [
     {
         name : "Велосипед",
         price : 40000
-    },
+    },     // (el)- один объект массива, один элемент
     {
         name : "Самокат",
         price : 15000
@@ -34,56 +34,93 @@ form.addEventListener("submit", function(event) {
     event.preventDefault()
     //console.log(event)
     //console.log("отправлено")
-    console.log(productName.value)
-    console.log(productPrice.value)
+    // console.log(productName.value)
+    // console.log(productPrice.value)
+
+    //создаем объект с продуктом, который хочет добавить пользователь ,
+    // на основе введенных данных
+    let newProduct = {
+        name: productName.value,
+        price: Number(productPrice.value)
+    }
+   // console.log(newProduct) // переменная видна только внутри блока кода
+   
+   //добавляем объект newProduct в массив products
+   products.push(newProduct)
+   //console.log(products)
+
+    renderProducts(products)
+    onHover()
+    clearInputs()
+
 
 })
 
+function clearInputs() {
+    productName.value = ''
+    productPrice.value = ''
+}
+//________________________________________________
+//________________________________________________
 // Создать функцию renderProducts(), которая принимает массив arr в аргументы
 // Функция должна для каждого элемента массива arr отобразить разметку
 // В получаемом массиве лежат объекты со свойтсвами name, price
 // forEach(), innerHTML. Вызвать функцию с массивом products. renderProducts(products)
 // `${name}${price}`
-
+//_____________________
 function renderProducts(arr) {
+    productsWrapper.innerHTML = ''
+
     arr.forEach(function(el) {
         productsWrapper.innerHTML += `
         <div class="product">
         <p class="product-name">${el.name}</p>
         <p class="product-price">${el.price}</p>
+        <button class="delete-btn">X</button>
         </div>
         `
     })
 }
 renderProducts(products)
 
+//Функция, которая позволяет, при наведении на элемент, отображать кнопку удаления
+function onHover() {
+    let product = document.querySelectorAll(".product")
+    product.forEach(function(el) {
+        el.addEventListener("mouseover", function() {
+            el.querySelector(".delete-btn").style.display = "block"
+        })
+        el.addEventListener("mouseout", function() {
+            el.querySelector(".delete-btn").style.display = "none"
+        })
+    })
+}
+onHover()
+//Функция удаления элемента при нажатии
+function deleteProduct() {
+    let deleteBtns = document.querySelectorAll(".delete-btn")
+    deleteBtns.forEach(function(btn) {
+        btn.addEventListener("click", function() {
+            btn.parentElement.remove() //удаляем родителя кнопки удаления
+        
+        })
+    })
+}
+deleteProduct()
 
-// если вызывать функцию только 1 раз, но не подействует если появятся ещё другие теги 
-/*  
-    products.forEach(function renderProducts(arr) {
-    productsWrapper.innerHTML += 
-    `<div class="product">
-        <p class="product-name">${arr.name}</p>
-        <p class="product-price">${arr.price}</p>
-    </div> `
-    }) 
-*/
 
-
-
-
-
+//_____________________________________________________________
 /* 
     При любом событии создается объект события - event, внутри которого лежит информация об этом событии
     -кто вызвал событие, -какие компоненты участвуют, элементы формы со значениями и т.д
     Иногда нужно отменять действия по умолчанию при создании события с помощью event.preventDefault()
 */
 
-
-
 /* 
 <div class="product">
 <p class="product-name">велосипед</p>
 <p class="product-price">40000</p>
 </div> */
+//____________________________________________
+
 
